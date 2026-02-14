c0c7b10ef40173f1cd88007c55a0a49ba9906603
Urffin-Juss <mellondefall@gmail.com>
2026-02-14 09:42:43 +0300

    add class client

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=25, unique=True, db_index=True)
+    dob = models.DateField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    contact = models.TextField(null=True, blank=True)
+    pets = models.TextField(null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.phone
+
+
+
