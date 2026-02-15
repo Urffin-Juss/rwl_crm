94bafa1f24ee4d8b97f646aba77620799989cbf7
Urffin-Juss <mellondefall@gmail.com>
2026-02-15 08:48:00 +0300

    win acess logic

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from unicodedata import name
 
 from apps.orders.models import Order, OrderItem
 
@@ -14,6 +15,49 @@ class OrderAdmin(admin.ModelAdmin):
     list_filter = ('status','event', 'assigned_packer')
     search_fields = ('client__phone', 'client__name', 'event__name')
     inlines = [OrderItemInline]
+
+    def _has_full_access(self, request):
+        """True если пользователь имеет полный доступ"""
+        return any([
+            request.user.is_superuser,
+            request.user.groups.filter(name='Owner').exists(),
+            request.user.groups.filter(name='Admin').exists()
+        ])
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if self._has_full_access(request):
+            return qs
+        return qs.filter(assigned_packer=request.user)
+
+    def has_add_permission(self, request):
+        return self._has_full_access(request)
+
+    def has_delete_permission(self, request, obj=None):
+        return self._has_full_access(request)
+
+
+    def has_change_permission(self, request, obj=None):
+      return self._has_full_access(request)
+
+
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return []
+        elif request.user.groups.filter(name='Owner').exists():
+            return []
+        elif request.user.groups.filter(name='Admin').exists():
+            return []
+        else:
+            return ['client', 'event', 'assigned_packer', 'status', 'payment_status']
+
+
+
+
+
+
+
+
 
 
 
--- apps/tasks/admin.py
+++ apps/tasks/admin.py
@@ -10,3 +10,19 @@ class TaskAdmin(admin.ModelAdmin):
     search_fields = ('title', 'status', 'description',)
 
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+
+        if request.user.is_superuser:
+            return qs
+
+
+        full_access_groups = ['Owner','Аdmin']
+        if request.user.groups.filter(name__in=full_access_groups).exists():
+            return qs
+
+
+        return qs.filter(assigned=request.user)
+
+
