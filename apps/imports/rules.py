888d3fbf04eeb13c08621fef1f2fba5392a35ebb
Urffin-Juss <mellondefall@gmail.com>
2026-02-17 15:32:40 +0300

    add rules

---
+++ apps/imports/rules.py
@@ -0,0 +1,27 @@
+
+EXACT = {
+    "last_name": "Фамилия",
+    "first_name": "Имя",
+    "middle_name": "Отчество",
+    "dob": "Дата рождения",
+    "gender": "Пол",
+    "phone": "Мобильный телефон",
+    "email": "Электронная почта",
+    "city": "Город",
+    "index": "Индекс",
+    "region": "Регион",
+    "street": "Улица",
+    "house": "Дом",
+    "flat": "Квартира",
+    "distance": "Дистанция",
+    "category": "Категория",
+    "registration_date": "Дата регистрации",
+    "payment_status": "Статус оплаты",
+    "payment_type": "Тип оплаты",
+}
+
+CONTAINS = {
+    "delivery_address_text": "укажите адрес для доставки",
+    "contact_text": "как и где с вами связаться",
+    "pets_text": "кошечки или собачки",
+}
