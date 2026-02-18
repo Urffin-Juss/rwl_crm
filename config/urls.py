e0a9194512bd74831ead8baf41be908a2ab1251d
Urffin-Juss <mellondefall@gmail.com>
2026-02-18 16:22:05 +0300

    work with front

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,21 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from django.contrib.auth import views as auth_views
+
+from apps.webui import views as webui_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('', webui_views.landing, name='landing'),
+    path('workspace/', webui_views.workspace, name='workspace'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='webui/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
 
 if settings.DEBUG:
