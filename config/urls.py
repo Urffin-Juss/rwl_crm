54609bbdbca0f387303b997231b6d04a0f2a01de
Urffin-Juss <im@maxsuns.ru>
2026-08-28 14:54:21 +0300

    add event urls in config

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.contrib.auth import views as auth_views
 
@@ -17,6 +17,9 @@ urlpatterns = [
 
     path('login/', auth_views.LoginView.as_view(template_name='webui/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+
+    path('api/', include('apps.events.urls')),
 ]
 
 if settings.DEBUG:
