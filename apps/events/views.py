527a68aa3d9c37fefccd4cf618fe9af9a5140a0c
Urffin-Juss <im@maxsuns.ru>
2026-08-29 17:27:10 +0300

    some func

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import ListAPIView, CreateAPIView
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 from apps.events.models import Event
 from apps.events.serializers import EventSerializer, EventParticipationSerializer
@@ -9,6 +11,8 @@ class EventListAPIView(ListAPIView):
     serializer_class = EventSerializer
 
 
+
+@method_decorator(csrf_exempt, name='dispatch')
 class EventParticipationCreateAPIView(CreateAPIView):
     queryset = Event.objects.all()
     serializer_class = EventParticipationSerializer
