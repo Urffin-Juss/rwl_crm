6b2f62ac4b93b538e8708bb719333c139225ff9f
Urffin-Juss <im@maxsuns.ru>
2026-08-29 11:16:34 +0300

    add urls and APIView

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.events.views import EventListAPIView
+from apps.events.views import EventListAPIView, EventParticipationCreateAPIView
 
 
 urlpatterns = [
     path('events/', EventListAPIView.as_view(), name='event-list'),
+    path('api/participations/', EventParticipationCreateAPIView.as_view(), name='participation-create'),
 ]
--- apps/events/views.py
+++ apps/events/views.py
@@ -1,9 +1,14 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 
 from apps.events.models import Event
-from apps.events.serializers import EventSerializer
+from apps.events.serializers import EventSerializer, EventParticipationSerializer
 
 
 class EventListAPIView(ListAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+
+class EventParticipationCreateAPIView(CreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventParticipationSerializer
