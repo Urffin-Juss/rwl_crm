c108679b54bfc3d895f1dbee1b577bc93151d606
Urffin-Juss <im@maxsuns.ru>
2026-08-31 10:57:02 +0300

    check idenity user

--- static/webui/calendar.js
+++ static/webui/calendar.js
@@ -66,6 +66,17 @@ if (tg) {
     console.log('Telegram WebApp не найден');
 }
 
+if (tg?.initDataUnsafe?.user) {
+
+    const user = tg.initDataUnsafe.user;
+
+    alert(
+        `Telegram ID: ${user.id}\n` +
+        `Имя: ${user.first_name}\n` +
+        `Username: ${user.username}`
+    );
+}
+
 
 
 
