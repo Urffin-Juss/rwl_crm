abe7163cc8b37800c8c13500c160c92316a2b37a
Urffin-Juss <im@maxsuns.ru>
2026-09-02 14:08:55 +0300

    add admin registr

--- apps/parser/admin.py
+++ apps/parser/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ParserSource, ParserRawEvent
+
+
+@admin.register(ParserSource)
+class ParserSourceAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "slug",
+        "is_active",
+        "updated_at",
+    )
+
+    list_filter = (
+        "is_active",
+    )
+
+    search_fields = (
+        "name",
+        "slug",
+    )
+
+
+@admin.register(ParserRawEvent)
+class ParserRawEventAdmin(admin.ModelAdmin):
+    list_display = (
+        "raw_name",
+        "source",
+        "raw_city",
+        "raw_date",
+        "parsed_at",
+    )
+
+    list_filter = (
+        "source",
+    )
+
+    search_fields = (
+        "raw_name",
+        "raw_city",
+        "external_id",
+        "source_url",
+    )
+
+    ordering = (
+        "-parsed_at",
+    )
