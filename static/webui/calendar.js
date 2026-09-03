098b0f0ed3be42182d4b76fa0587b5273ae00683
Urffin-Juss <im@maxsuns.ru>
2026-09-03 09:40:26 +0300

    Checkpoint. End parser. Tests is ok

--- static/webui/calendar.js
+++ static/webui/calendar.js
@@ -1789,7 +1789,7 @@ function showMembershipGate() {
 
 /*
     ========================================
-    START (for commit)
+    START
     ========================================
 */
 async function startApp() {
